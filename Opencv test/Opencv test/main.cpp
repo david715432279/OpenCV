89125eafccfc61d05521216a0ccd77a47c9227bb
David Huang <davidhuang715432279@gmail.com>
2013-07-11 09:47:20 +0800

    first commit

---
+++ Opencv test/Opencv test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Opencv test
+//
+//  Created by luodan0318 on 13-7-11.
+//  Copyright (c) 2013年 luodan0318. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
